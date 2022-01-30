c8784fff9880c460b7eb5020c0b603be6baabfab
masoudtestdeploy <95133886+masoudtestdeploy@users.noreply.github.com>
2022-01-31 01:07:00 +0330

    Update main.py

--- Instaloader/main.py
+++ Instaloader/main.py
@@ -12,7 +12,7 @@ async def main(_, msg):
     if 'instagram.com' not in msg.text:
         return
     status = await msg.reply('Please Wait...', quote=True)
-    pattern = re.compile(r'^(https?:[/][/])?(www\.)?instagram.com[/](p|reel)[/]([A-Za-z0-9-_]+)')
+    pattern = re.compile(r'^(https?:[/][/])?(www\.)?instagram.com[/](p|reel|tv)[/]([A-Za-z0-9-_]+)')
     try:
         matches = pattern.search(msg.text)
         post_id = matches.group(4)
@@ -40,9 +40,9 @@ async def main(_, msg):
             return
         if len(photos+videos) == 1:
             if caption:
-                caption += "\n\nBy @StarkBots"
+                caption += "\n\nBy @masoudartwork"
             else:
-                caption = "By @StarkBots"
+                caption = "By @masoudartwork"
             if photos:
                 for photo in photos:
                     await msg.reply_photo(photo, caption)
@@ -57,7 +57,7 @@ async def main(_, msg):
                 for video in videos:
                     await msg.reply_video(video)
             if caption:
-                await msg.reply(f"**POST CAPTION : **\n\n{caption} \n\nBy @StarkBots")
+                await msg.reply(f"**POST CAPTION : **\n\n{caption} \n\nBy @masoudartwork")
         await status.delete()
         shutil.rmtree(path)
     except AttributeError:
